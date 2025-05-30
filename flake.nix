efd534fdbddb2a5dc186ed4894b61e43ff98b11f
cocvu <cocvu@nightcord.unknown.tld>
2025-05-30 06:58:53 +0000

    add precommit hook (nixfmt)

--- flake.nix
+++ flake.nix
@@ -4,26 +4,52 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixvim.url = "github:nix-community/nixvim";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = { self, flake-parts, nixvim, ... }@inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
+  outputs =
+    {
+      self,
+      flake-parts,
+      nixpkgs,
+      nixvim,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [
         "aarch64-darwin"
         "aarch64-linux"
         "x86_64-darwin"
         "x86_64-linux"
       ];
-
-      perSystem = { pkgs, system, ... }: let
-        config = import ./config;
-        nixvim' = nixvim.legacyPackages."${system}";
-        nvim = nixvim'.makeNixvim config;
-      in {
-        packages = {
-          inherit nvim;
-          default = nvim;
+      perSystem =
+        { pkgs, system, ... }:
+        let
+          config = import ./config;
+          nixvim' = nixvim.legacyPackages."${system}";
+          nvim = nixvim'.makeNixvim config;
+        in
+        {
+          checks = {
+            inherit inputs system;
+            pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                nixfmt-rfc-style.enable = true;
+              };
+            };
+          };
+          devShells = {
+            inherit system;
+            default = pkgs.mkShell {
+              inherit (self.checks.${system}.pre-commit-check) shellHook;
+              buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+            };
+          };
+          packages = {
+            inherit nvim;
+            default = nvim;
+          };
         };
-      };
-  };
+    };
 }
