6a290092c2c3ccd838eafc25d4348f61fde9b9c7
cocvu <cocvu@nightcord.unknown.tld>
2025-08-13 09:13:48 +0000

    indent 2 spaces in beancount files

--- config/files.nix
+++ config/files.nix
@@ -1,10 +1,12 @@
-let 
+let
   indent2 = {
     opts.expandtab = true;
     opts.shiftwidth = 2;
     opts.tabstop = 2;
   };
-in {
+in
+{
+  files."ftplugin/beancount.lua" = indent2;
   files."ftplugin/javascript.lua" = indent2;
   files."ftplugin/json.lua" = indent2;
   files."ftplugin/haskell.lua" = indent2;
