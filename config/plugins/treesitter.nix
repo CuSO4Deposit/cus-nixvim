999c0d146c0aa80435f3ad7d69be934b0d4213bb
cocvu <cocvu@nightcord.unknown.tld>
2025-12-02 10:35:20 +0000

    enable highlight in treesitter; fix python indent

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -1,3 +1,19 @@
 {
   enable = true;
+  settings = {
+    highlight = {
+      additional_vim_regex_highlighting = true;
+      enable = true;
+    };
+    incremental_selection = {
+      enable = true;
+      keymaps = {
+        init_selection = "<A-o>";
+        node_incremental = "<A-o>";
+        node_decremental = "<A-i>";
+        scope_incremental = "<A-O>";
+      };
+    };
+    indent.enable = true;
+  };
 }
